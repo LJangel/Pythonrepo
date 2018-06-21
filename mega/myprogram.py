762160c0fff670f948ebc370846fe0734b1508f8
LJangel <LJ@toneangel.com>
2018-06-20 20:23:06 -0400

    add myprogram.py from mega course

--- mega/myprogram.py
+++ mega/myprogram.py
@@ -1,1 +1,8 @@
-print("hello", 7, 8)
+# greeting2 = input("write a greeting: ")
+# print(greeting2)
+# a = 2
+# b = 3
+# print(int(a+b))
+age = input("enter your age: ")
+new_age = int(age) + 50
+print(new_age)
